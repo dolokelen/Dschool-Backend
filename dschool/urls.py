a44832af42f0724dccf921494b26e56ed2825d0b
dolokelen <dolokelen1@gmail.com>
2023-08-28 08:13:45 +0800

    Installed and configured DJOSER and Django Restframework Simple JWT for Authentication

--- dschool/urls.py
+++ dschool/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
-    path('', include('evaluation.urls'))
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('', include('evaluation.urls')),
 ]
